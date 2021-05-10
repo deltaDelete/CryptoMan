fab6dbe687a9ceef2636737772f11090e4cabd44
deltaDelete <40860312+deltaDelete@users.noreply.github.com>
2021-05-10 18:58:57 +0700

    Initial commit

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CryptoMan
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        bool isMenuOpen = false;
+        public MainWindow()
+        {
+            InitializeComponent();
+
+        }
+
+        // Раскрыть/Свернуть меню
+        private void Menu_Click(object sender, RoutedEventArgs e)
+        {
+            CubicEase easing = new CubicEase();
+            easing.EasingMode = EasingMode.EaseInOut;
+
+            //Анимация меню раскрытие
+            DoubleAnimation MenuGridAnimationOpen = new DoubleAnimation();
+            MenuGridAnimationOpen.From = MenuGrid.MinWidth;
+            MenuGridAnimationOpen.To = MenuGrid.MaxWidth;
+            MenuGridAnimationOpen.Duration = TimeSpan.FromSeconds(0.1);
+            MenuGridAnimationOpen.EasingFunction = easing;
+
+            //Анимация меню сворачивание
+            DoubleAnimation MenuGridAnimationClose = new DoubleAnimation();
+            MenuGridAnimationClose.From = MenuGrid.MaxWidth;
+            MenuGridAnimationClose.To = MenuGrid.MinWidth;
+            MenuGridAnimationClose.Duration = TimeSpan.FromSeconds(0.1);
+            MenuGridAnimationClose.EasingFunction = easing;
+
+            if (isMenuOpen == true)
+            {
+                //MenuGrid.Width = MenuGrid.MinWidth;
+                isMenuOpen = false;
+                MenuGrid.BeginAnimation(Grid.WidthProperty, MenuGridAnimationClose);
+            } else if (isMenuOpen == false) {
+                //MenuGrid.Width = MenuGrid.MaxWidth;
+                isMenuOpen = true;
+                MenuGrid.BeginAnimation(Grid.WidthProperty, MenuGridAnimationOpen);
+            }
+        }
+        
+        //Кнопки меню
+        private void Nav2P1(object sender, RoutedEventArgs e)
+        {
+            Frame1.Source = new Uri("Pages/Page1.xaml", UriKind.Relative);
+
+        }
+        private void Nav2P2(object sender, RoutedEventArgs e)
+        {
+            Frame1.Source = new Uri("Pages/Page2.xaml", UriKind.Relative);
+
+        }
+    }
+}
